d49300fc5a4a9a20d9ec26c006b26413095c46fe
Radosław Cybulski <radoslaw.cybulski@scylladb.com>
2026-03-21 08:16:56 +0100

    moved NativeClassInterface to it's own file

---
+++ owca-script/native_class_interface.h
@@ -0,0 +1,27 @@
+#ifndef RC_OWCA_SCRIPT_NATIVE_CLASS_INTERFACE_H
+#define RC_OWCA_SCRIPT_NATIVE_CLASS_INTERFACE_H
+
+namespace OwcaScript {
+	class OwcaValue;
+	class GenerationGC;
+	class OwcaVM;
+	
+	struct NativeClassInterface {
+		virtual ~NativeClassInterface() = default;
+
+		virtual void initialize_storage(void* ptr, size_t s) = 0;
+		virtual void destroy_storage(void* ptr, size_t s) = 0;
+		virtual void gc_mark_members(void* ptr, size_t s, OwcaVM vm, GenerationGC generation_gc) = 0;
+		virtual size_t native_storage_size() = 0;
+		virtual bool get_member(OwcaVM vm, std::string_view, std::span<char> native_storage, OwcaValue &) { return false; }
+		virtual bool set_member(OwcaVM vm, std::string_view, std::span<char> native_storage, const OwcaValue &) { return false; }
+	};
+	template <typename T> struct NativeClassInterfaceSimpleImplementation : public NativeClassInterface {
+		void initialize_storage(void* ptr, size_t s) override { new (ptr) T(); }
+		void destroy_storage(void* ptr, size_t s) override { ((T*)ptr)->~T(); }
+		void gc_mark_members(void* ptr, size_t s, OwcaVM vm, GenerationGC generation_gc) override { ((T*)ptr)->gc_mark(vm, generation_gc); }
+		size_t native_storage_size() override { return sizeof(T); }
+	};
+}
+
+#endif
--- owca-script/owca_class.h
+++ owca-script/owca_class.h
@@ -4,6 +4,7 @@
 #include "stdafx.h"
 #include <new>
 #include "owca_vm.h"
+#include "native_class_interface.h"
 
 namespace OwcaScript {
 	class OwcaValue;
@@ -13,23 +14,6 @@ namespace OwcaScript {
 	namespace Internal {
 		struct Class;
 	}
-
-	struct NativeClassInterface {
-		virtual ~NativeClassInterface() = default;
-
-		virtual void initialize_storage(void* ptr, size_t s) = 0;
-		virtual void destroy_storage(void* ptr, size_t s) = 0;
-		virtual void gc_mark_members(void* ptr, size_t s, OwcaVM vm, GenerationGC generation_gc) = 0;
-		virtual size_t native_storage_size() = 0;
-		virtual bool get_member(OwcaVM vm, std::string_view, std::span<char> native_storage, OwcaValue &) { return false; }
-		virtual bool set_member(OwcaVM vm, std::string_view, std::span<char> native_storage, const OwcaValue &) { return false; }
-	};
-	template <typename T> struct NativeClassInterfaceSimpleImplementation : public NativeClassInterface {
-		void initialize_storage(void* ptr, size_t s) override { new (ptr) T(); }
-		void destroy_storage(void* ptr, size_t s) override { ((T*)ptr)->~T(); }
-		void gc_mark_members(void* ptr, size_t s, OwcaVM vm, GenerationGC generation_gc) override { ((T*)ptr)->gc_mark(vm, generation_gc); }
-		size_t native_storage_size() override { return sizeof(T); }
-	};
 
 	class OwcaClass {
 		Internal::Class* object;
