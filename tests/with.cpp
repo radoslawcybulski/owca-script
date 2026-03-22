871ac7bdf52e32b1df1990753b3542b62e0bdc0f
Radosław Cybulski <radoslaw.cybulski@scylladb.com>
2026-03-22 10:24:46 +0100

    add test for with statement

--- tests/with.cpp
+++ tests/with.cpp
@@ -15,7 +15,7 @@ static auto run_with(std::string code_text, OwcaValue add_val)
     auto val = vm.execute(code, vm.create_map(map_data));
     return std::string{ val.as_string(vm).text() };
 }
-TEST_F(WithTest, simple1)
+TEST_F(WithTest, simple)
 {
     ASSERT_EQ(run_with(R"(
 
@@ -52,6 +52,57 @@ with(q = Q(1)) {
         queue.push_back(w.v);
     }
 }
+
+s = '';
+for(q = queue) {
+    s = s + String(q);
+}
+return s;
+	)", 0), std::string_view{ "B1C1A5E5B2C2A6F6D2D1" });
+}
+
+TEST_F(WithTest, with_exc)
+{
+    ASSERT_EQ(run_with(R"(
+
+queue = [];
+class W {
+    function __init__(self, v) {
+        self.v = v + 4;
+        queue.push_back('A');
+        queue.push_back(self.v);
+    }
+}
+class Q {
+    function __init__(self, v) {
+        self.v = v;
+        queue.push_back('B');
+        queue.push_back(self.v);
+    }
+    function __enter__(self) {
+        queue.push_back('C');
+        queue.push_back(self.v);
+        return W(self.v);
+    }
+    function __exit__(self) {
+        queue.push_back('D');
+        queue.push_back(self.v);
+    }
+}
+
+try {
+    with(q = Q(1)) {
+        queue.push_back('E');
+        queue.push_back(q.v);
+        with(w = Q(2)) {
+            queue.push_back('F');
+            queue.push_back(w.v);
+            throw Exception("err");
+        }
+    }
+    return 'no exception';
+}
+catch(Exception) {}
 
 s = '';
 for(q = queue) {
